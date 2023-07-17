245a0accb040bd11308b5ced634d99c476936450
Fredy Fredy <45609912+fredfoxtrot@users.noreply.github.com>
2023-07-16 22:50:53 -0400

    first commit

---
+++ upgrade-tech-challenge/src/main/java/com/upgrade/challenge/UpgradeTechChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.upgrade.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradeTechChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradeTechChallengeApplication.class, args);
+	}
+
+}
---
+++ upgrade-tech-challenge/src/test/java/com/upgrade/challenge/UpgradeTechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrade.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpgradeTechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
